8397424460f48d63b000cce7d45f48ae8df083d6
Prakhar Tripathi <prakhar.tripathi@mirumagency.com>
2020-02-03 13:02:16 +0530

    added update status api call

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# teravalve-serverless
+# TeraValve Serverless
+
+
+## Endpoints created 
+1. https://us-central1-teravalve-90327.cloudfunctions.net/helloWorld 
+	- Normal Hello world API
+2. https://us-central1-teravalve-90327.cloudfunctions.net/updateStatus
+	- POST method
+	- Body in the form of - {"alarmEngaged": true/false}
+	- Response like - {alarmEngaged - true/false}   			
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,27 @@
 const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
+const admin = require("firebase-admin");
+
+admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
+});
+
+exports.updateStatus = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if (req.method !== "POST") {
+      return res.status(401).json({
+        message: "Not allowed"
+      });
+    }
+
+    console.log(req.body);
+
+    return res.status(200).json({
+      received: req.body.alarmEngaged
+    });
+
+    // const item = req.body.item;
+  });
 });
